a1bdb0766f15f41b663359b851f0492897c3d57a
Khan,Mohammad Nazibul M.N.K <nazibulkabir.khan@student.fontys.nl>
2022-02-25 21:27:37 +0100

    more mapping for CRUD

--- ListenUp/src/main/java/com/listenup/ListenUp/controller/UserController.java
+++ ListenUp/src/main/java/com/listenup/ListenUp/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
     private UserManagment managment = new UserManagmentImp(db);
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllStudents() {
+    public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = managment.getUsers();
 
         if(users != null) {
@@ -30,7 +30,7 @@ public class UserController {
         }
     }
     @GetMapping("{id}")
-    public ResponseEntity<User> getStudentPath(@PathVariable(value = "id") int id) {
+    public ResponseEntity<User> getUserPath(@PathVariable(value = "id") int id) {
         User student = managment.getUserByID(id);
 
         if(student != null) {
@@ -38,5 +38,30 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+    @PostMapping()
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        if (!managment.createAccount(user)){
+            String entity =  "User with this email " + user.getEmail() + " already exists.";
+            return new ResponseEntity(entity, HttpStatus.CONFLICT);
+        } else {
+            String url = "user" + "/" + user.getId();
+            URI uri = URI.create(url);
+            return new ResponseEntity(uri,HttpStatus.CREATED);
+        }
+    }
+    @PutMapping()
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        if (managment.updateAccount(user)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return new ResponseEntity("Please provide a valid country code.",HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity deletePost(@PathVariable int id) {
+        managment.deleteAccount(id);
+        return ResponseEntity.ok().build();
+
     }
 }
