c82be46e412ef8009438a921a57c6ae9b3b93b1f
Khan,Mohammad Nazibul M.N.K <nazibulkabir.khan@student.fontys.nl>
2022-03-18 17:33:46 +0100

    playlist 3 layer design

--- ListenUp/src/main/java/com/listenup/ListenUp/business/PlaylistManagement.java
+++ ListenUp/src/main/java/com/listenup/ListenUp/business/PlaylistManagement.java
@@ -1,4 +1,13 @@
 package com.listenup.ListenUp.business;
 
+import com.listenup.ListenUp.model.Playlist;
+
+import java.util.List;
+
 public interface PlaylistManagement {
+    boolean addPlaylist(Playlist playlist);
+    List<Playlist> getPlaylists();
+    boolean editPlaylist(Playlist playlist);
+    boolean deletePlaylist(int id);
+    Playlist getPlaylist(int id);
 }
--- ListenUp/src/main/java/com/listenup/ListenUp/persistence/DBPlaylist.java
+++ ListenUp/src/main/java/com/listenup/ListenUp/persistence/DBPlaylist.java
@@ -1,4 +1,12 @@
 package com.listenup.ListenUp.persistence;
 
+import com.listenup.ListenUp.model.Playlist;
+
+import java.util.List;
+
 public interface DBPlaylist {
+    boolean addPlaylist(int id, String name, boolean isPublic);
+    List<Playlist> getPlaylists();
+    boolean editPlaylist(int id, String name, boolean isPublic);
+    boolean deletePlaylist(int id);
 }
